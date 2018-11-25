cb3ca6a6978af3fa9a46dace4cd3022861fde4a3
ilcnus <42750060+ilcnus@users.noreply.github.com>
2018-11-25 15:57:07 +0800

    countdown

---
+++ countdown/js/global.js
@@ -0,0 +1,23 @@
+﻿$( function() {
+	$.backstretch('https://i.loli.net/2018/11/25/5bfa558c7a87b.jpg');
+	var endDate = "January 5, 2019 00:00:00";
+	$('.countdown.simple').countdown({ date: endDate });
+	$('.countdown.styled').countdown({
+	  date: endDate,
+	  render: function(data) {
+		$(this.el).html("<div>" + this.leadingZeros(data.days, 2) + " <span>天</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>时</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>分</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>秒</span></div>");
+	  }
+	});
+	$('.countdown.callback').countdown({
+	  date: +(new Date) + 10000,
+	  render: function(data) {
+		$(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+	  },
+	  onEnd: function() {
+		$(this.el).addClass('ended');
+	  }
+	}).on("click", function() {
+	  $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+	});
+
+});
